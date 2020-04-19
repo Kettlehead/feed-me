b9e5adb32b5e7795c792309629a04f4977fbc24a
Liam Hawks <liam@Continuity.local>
2020-04-19 11:13:06 +0100

    feat: Add game over message & restart option

--- src/scenes/GameOver.js
+++ src/scenes/GameOver.js
@@ -6,5 +6,28 @@ export default class Game extends Phaser.Scene {
     super({ key: SCENE.GAME_OVER });
   }
 
-  create() {}
+  create() {
+    const finalText = this.add.bitmapText(
+      20,
+      14,
+      "alagard",
+      `A terrible shame that you have died. Press any key to restart`
+    );
+    Phaser.Display.Align.In.Center(
+      finalText,
+      this.add.zone(
+        this.cameras.main.centerX,
+        this.cameras.main.centerY,
+        this.cameras.main.width,
+        this.cameras.main.height
+      )
+    );
+    this.input.keyboard.on("keydown", () => {
+      const cam = this.cameras.main;
+      cam.fade(500, 0, 0, 0);
+      cam.once("camerafadeoutcomplete", () => {
+        this.scene.start(SCENE.INTRO);
+      });
+    });
+  }
 }
